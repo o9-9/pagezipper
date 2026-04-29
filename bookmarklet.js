8d6dc8800f63e89df800241d9b60810466a2a918
o9 <shno59.9009@gmail.com>
2026-04-30 02:47:46 +0300

    Update bookmarklet script source and remove outdated dependencies from package.json

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,8 +4,9 @@ javascript: (function () {
   } else {
     window._page_zipper_is_bookmarklet = true
     window._page_zipper = document.createElement('script')
-    window._page_zipper.type = 'bookmarklet'
-    window._page_zipper.src = 'https://raw.githubusercontent.com/o9-9/pagezipper/refs/heads/main/img/'
+    window._page_zipper.type = 'text/javascript'
+    window._page_zipper.src =
+      'https://raw.githubusercontent.com/o9-9/pagezipper/refs/heads/main/dist/pagezipper.js'
     document.getElementsByTagName('head')[0].appendChild(window._page_zipper)
   }
 })()
